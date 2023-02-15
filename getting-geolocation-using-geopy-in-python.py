92efaa085e1ab319053f5f9c788ce3acefde6f0b
domssab <saballo.chantaldominique@gmail.com>
2023-02-15 22:23:43 +0800

    Trying to loop back the program to beginning

--- getting-geolocation-using-geopy-in-python.py
+++ getting-geolocation-using-geopy-in-python.py
@@ -2,41 +2,48 @@
 # import the geopy library
 from geopy.geocoders import Nominatim
 
+def start():
 # make user choose between location or coordinates
-print("Here are your options:"
-      "\n1. Location"
-      "\n2. Coordinates",
-      "\n3. Exit")
-options = input("Location or Coordinates?: ")
+    print("Here are your options:"
+        "\n1. Location"
+        "\n2. Coordinates",
+        "\n3. Exit")
+    while True:
+        options = input("Location or Coordinates?: ")
 
 # if the user chose option 1
-if options == '1':
+        if options == '1':
     # call the Nominatim tool
-    loc = Nominatim(user_agent="GetLoc")
+            loc = Nominatim(user_agent="GetLoc")
 
     # asking user to input location
-    location = input("Location: ")
-    getLoc = loc.geocode(location)
+            location = input("Location: ")
+            getLoc = loc.geocode(location)
 
     # printing address
-    print(getLoc.address)
+            print(getLoc.address)
 
     # printing latitude and longitude
-    print("Latitude = ", getLoc.latitude, "\n")
-    print("Longitude = ", getLoc.longitude)
+            print("Latitude = ", getLoc.latitude, "\n")
+            print("Longitude = ", getLoc.longitude)
 
 # if the user chose option 2
-if options == '2':
+        if options == '2':
     # call the Nominatim Tool
-    geoLoc = Nominatim(user_agent="GetLoc")
+            geoLoc = Nominatim(user_agent="GetLoc")
 
     # asking user to input the coordinates
-    coordinate = input("Coordinates: ")
-    locname = geoLoc.reverse(coordinate)
+            coordinate = input("Coordinates: ")
+            locname = geoLoc.reverse(coordinate)
 
     # printing the address/location name
-    print(locname.address)
+            print(locname.address)
 
 # if the user chose option 3
-if options == '3':
-    print("Thank you!")
+        if options == '3':
+            print("Thank you!")
+        break
+
+    else:
+        print("Make sure you input the correct number.")
+start()
